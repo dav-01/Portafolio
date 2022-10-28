91806ea89bb4ebddf8f50ffa81fe2f54a5d55e1b
David Cabrera <cabrerabuelvasd@gmail.com>
2022-10-27 19:12:38 -0500

    cambios

--- main.js
+++ main.js
@@ -1,28 +1,21 @@
-$(function(){
+$(function () {
     var texto = "David Cabrera";
-    maquina("typer",texto,100,0);
-   });
-   
-   function maquina(contenedor,texto,intervalo,n){
-    var i=0,
-     // Creamos el timer
-     timer = setInterval(function() {
-     if ( i<texto.length ) {
-      // Si NO hemos llegado al final del texto..
-      // Vamos añadiendo letra por letra y la _ al final.
-      $("#"+contenedor).html(texto.substr(0,i++) + "_");
-     } else {
-      // En caso contrario..
-      // Salimos del Timer y quitamos la barra baja (_)
-      clearInterval(timer);
-      $("#"+contenedor).html(texto);
-      // Auto invocamos la rutina n veces (0 para infinito)
-      if ( --n!=0 ) {
-       setTimeout(function() {
-        maquina(contenedor,texto,intervalo,n);
-       },3600);
-      }
-     }
-    },intervalo);
-   };
-   
+    maquina("typer", texto, 100, 0);
+});
+
+function maquina(contenedor, texto, intervalo, n) {
+    var i = 0,
+        timer = setInterval(function () {
+            if (i < texto.length) {
+                $("#" + contenedor).html(texto.substr(0, i++));
+            } else {
+                clearInterval(timer);
+                $("#" + contenedor).html(texto);
+                if (--n != 0) {
+                    setTimeout(function () {
+                        maquina(contenedor, texto, intervalo, n);
+                    }, 3600);
+                }
+            }
+        }, intervalo);
+};
